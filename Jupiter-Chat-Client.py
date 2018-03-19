3de739b7c0bb33529bc85227cf7c11fdc0f823e9
James Haywood <jhaywood@parktudor.org>
2018-03-19 14:26:31 -0400

    3/19/18 CSCLASS

--- Jupiter-Chat-Client.py
+++ Jupiter-Chat-Client.py
@@ -4,6 +4,19 @@ from threading import Thread
 import tkinter
 
 
+def about():
+    aboutwin = tkinter.Tk()
+    aboutwin.title("About")
+
+def servers():
+    print("")
+
+def settings():
+    print("")
+
+def helpb():
+    print("")
+
 def receive():
     while True:
         try:
@@ -32,31 +45,42 @@ top = tkinter.Tk()
 top.title("Jupiter Chat")
 
 messages_frame = tkinter.Frame(top)
+buttons_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()  # For the messages to be sent.
 my_msg.set("")
 
-jchat = tkinter.Label(messages_frame, text="Jupiter Chat", fg="orange", font=("TkDefaultFont", 20))
-jchat.pack(side=tkinter.LEFT, anchor=tkinter.NW)
+jchat = tkinter.Label(buttons_frame, text="MENU", fg="blue", font=("TkDefaultFont", 20))
+jchat.grid(row=0, column=3, sticky=tkinter.N)
 
 #sepA = tkinter.Seperator(messages_frame, orient=tkinter.HORIZONTAL)
 #sepA.pack(side=tkinter.LEFT, anchor=tkinter.NW) #doesnt work fix
 
-servb = tkinter.Button(messages_frame, text="Servers", width = 20)
-servb.pack(side=tkinter.LEFT, anchor=tkinter.NW)
+servb = tkinter.Button(buttons_frame, text="Servers", width = 20, command=servers)
+servb.grid(row=0, column=1, sticky=tkinter.NW)
+
+helpb = tkinter.Button(buttons_frame, text="Help", width = 20, command=helpb)
+helpb.grid(row=0, column=2, sticky=tkinter.NW)
+
+settb = tkinter.Button(buttons_frame, text="Settings", width = 20, command=settings)
+settb.grid(row=0, column=4, sticky=tkinter.NE)
+
+aboutb = tkinter.Button(buttons_frame, text="About", width = 20, command=about)
+aboutb.grid(row=0, column=5, sticky=tkinter.NE)
 
 scrollbar = tkinter.Scrollbar(messages_frame)  #scrollbar (vertical)
 #holds msgs
 msg_list = tkinter.Listbox(messages_frame, height=30, width=100, yscrollcommand=scrollbar.set)
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
-messages_frame.pack()
+scrollbar.grid(row=1, column=2, sticky=tkinter.E)
+msg_list.grid(row=1, column=1, sticky=tkinter.NE)
+msg_list.grid()
+buttons_frame.grid()
+messages_frame.grid()
 
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack(side=tkinter.RIGHT)
-entry_field = tkinter.Entry(top, width = 95, textvariable=my_msg)
+#send_button = tkinter.Button(top, text="Send", command=send)
+#send_button.grid(row=2, column=1, sticky=tkinter.SE)
+entry_field = tkinter.Entry(top, width = 100, textvariable=my_msg)
 entry_field.bind("<Return>", send)
-entry_field.pack(side=tkinter.RIGHT)
+entry_field.grid(row=2, column=0, sticky=tkinter.SW)
 
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
